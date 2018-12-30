8ed479e4fe6bca306758d98b657753d371cefe40
Paul Wilson <35266836+WilsoP@users.noreply.github.com>
2018-12-30 02:09:16 +0000

    Initial Commit and First Attemp

--- p3.py
+++ p3.py
@@ -20,6 +20,6 @@ while(loop > 600851470000):
         temp = loop
         print(temp)
         if(temp > max):
-            max = temp
+            max = # TEMP: 
 
 print(max)
---
+++ p6.py
@@ -0,0 +1,15 @@
+i = 0
+s = 0
+t = 0
+x = 0
+
+while (i < 100):
+    i+=1
+    s+= i**2
+    print(s)
+    t+= i
+
+t = t**2
+print(t)
+x = t-s
+print (x)
